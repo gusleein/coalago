3c95efc755acb91d99358cf05c7c6e0dc7672879
Mikhail Borovikov <dzen-it@ya.ru>
2018-04-02 21:24:23 +0700

    add tests for block mix

--- LayerARQ_test.go
+++ LayerARQ_test.go
@@ -12,10 +12,10 @@ import (
 )
 
 var (
-	portForTest        int = 1111
-	pathTestBlock1         = "/testblock1"
-	pathTestBlock2         = "/testblock2"
-	pathTestBlockMulti     = "/testblockmulti"
+	portForTest      int = 1111
+	pathTestBlock1       = "/testblock1"
+	pathTestBlock2       = "/testblock2"
+	pathTestBlockMix     = "/testblockmix"
 )
 
 func TestBlock1(t *testing.T) {
@@ -63,6 +63,29 @@ func TestBlock2(t *testing.T) {
 	}
 }
 
+func TestBlockMix(t *testing.T) {
+	var stringPayload string
+	for i := 0; i < 4; i++ {
+		stringPayload += strings.Repeat(fmt.Sprintf("%d", i), MAX_PAYLOAD_SIZE)
+	}
+
+	expectedResponse := []byte(stringPayload)
+
+	c := newClient()
+	s := newServer()
+	addResourceForBlockMix(s, expectedResponse)
+
+	message := newMessageForTestMix(expectedResponse)
+	address, _ := net.ResolveUDPAddr("udp", fmt.Sprintf("127.0.0.1:%d", portForTest))
+	resp, err := c.Send(message, address)
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(resp.Payload.Bytes(), expectedResponse) {
+		panic(fmt.Sprintf("Expected response: %s\n\nActual response: %s\n", expectedResponse, resp.Payload.Bytes()))
+	}
+}
+
 func newClient() *Coala {
 	return NewListen(0)
 }
@@ -91,6 +114,18 @@ func addResourceForBlock2(coala *Coala, expectedResponse []byte) *Coala {
 	return coala
 }
 
+func addResourceForBlockMix(coala *Coala, expectedPayload []byte) *Coala {
+	coala.AddPOSTResource(pathTestBlockMix, func(message *m.CoAPMessage) *resource.CoAPResourceHandlerResult {
+		if !bytes.Equal(message.Payload.Bytes(), expectedPayload) {
+			panic(fmt.Sprintf("Expected payload: %s\n\nActual payload: %s\n", expectedPayload, message.Payload.Bytes()))
+		}
+
+		resp := m.NewBytesPayload(expectedPayload)
+		return resource.NewResponse(resp, m.CoapCodeContent)
+	})
+	return coala
+}
+
 func newMessageForTestBlock1(expectedPayload []byte) *m.CoAPMessage {
 	message := m.NewCoAPMessage(m.CON, m.POST)
 	message.Payload = m.NewBytesPayload(expectedPayload)
@@ -101,5 +136,12 @@ func newMessageForTestBlock1(expectedPayload []byte) *m.CoAPMessage {
 func newMessageForTestBlock2() *m.CoAPMessage {
 	message := m.NewCoAPMessage(m.CON, m.GET)
 	message.SetURIPath(pathTestBlock2)
+	return message
+}
+
+func newMessageForTestMix(expectedPayload []byte) *m.CoAPMessage {
+	message := m.NewCoAPMessage(m.CON, m.POST)
+	message.Payload = m.NewBytesPayload(expectedPayload)
+	message.SetURIPath(pathTestBlockMix)
 	return message
 }
