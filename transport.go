9228ba7a8fd537f44298da198e4f899b9544658a
Mikhail Borovikov <dzen-it@ya.ru>
2019-04-16 11:57:32 +0700

    small ref

--- transport.go
+++ transport.go
@@ -518,30 +518,33 @@ func (sr *transport) ReceiveMessage(message *CoAPMessage, respHandler func(*CoAP
 }
 
 func (sr *transport) ReceiveOnce(respHandler func(*CoAPMessage, error)) {
-	readBuf := make([]byte, MTU+1)
-start:
-	n, senderAddr, err := sr.conn.Listen(readBuf)
-	if err != nil {
-		panic(err)
-	}
-	if n == 0 || n > MTU {
-		goto start
-	}
+	for {
+		readBuf := make([]byte, MTU+1)
+		
+		n, senderAddr, err := sr.conn.Listen(readBuf)
+		if err != nil {
+			panic(err)
+		}
+		if n == 0 || n > MTU {
+			continue
+		}
 
-	message, err := preparationReceivingBuffer(sr, readBuf[:n], senderAddr, "")
-	if err != nil {
-		goto start
-	}
+		message, err := preparationReceivingBuffer(sr, readBuf[:n], senderAddr, "")
+		if err != nil {
+			continue
+		}
+		message.Sender = senderAddr
 
-	message.Sender = senderAddr
+		if _, ok := handlersStateCache.Get(senderAddr.String() + string(message.Token)); ok {
+			return
+		}
 
-	if _, ok := handlersStateCache.Get(senderAddr.String() + string(message.Token)); ok {
+		handlersStateCache.SetDefault(senderAddr.String()+string(message.Token), struct{}{})
+		sr.messageHandlerSelector(message, respHandler)
+		handlersStateCache.Delete(message.Sender.String() + string(message.Token))
+
 		return
 	}
-
-	handlersStateCache.SetDefault(senderAddr.String()+string(message.Token), struct{}{})
-	sr.messageHandlerSelector(message, respHandler)
-	handlersStateCache.Delete(message.Sender.String() + string(message.Token))
 }
 
 func (sr *transport) messageHandlerSelector(message *CoAPMessage, respHandler func(*CoAPMessage, error)) {
