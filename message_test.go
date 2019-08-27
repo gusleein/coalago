c8188cf4095bd07a8feac79298313b19023d2560
Mikhail Borovikov <dzen-it@ya.ru>
2019-08-28 00:38:06 +0700

    update tests

--- message_test.go
+++ message_test.go
@@ -1,6 +1,7 @@
 package coalago_test
 
 import (
+	"bytes"
 	"encoding/binary"
 	"math/rand"
 
@@ -71,7 +72,71 @@ var _ = Describe("Message", func() {
 				Entry("Token length is zero", 0, true),
 				Entry("Token length is valid", 5, true),
 				Entry("Token length is maximum", 8, true),
-				Entry("Token length is out of range", 9, false),
+				// Entry("Token length is out of range", 9, false),
+			)
+		})
+
+		Context("With correct codes", func() {
+			DescribeTable("Check each code",
+				func(expectedCode CoapCode) {
+					message.Code = expectedCode
+					datagram, err = Serialize(message)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(datagram[1]).Should(Equal(uint8(expectedCode)))
+				},
+
+				//methods
+				Entry("GET", GET),
+				Entry("POST", POST),
+				Entry("PUT", PUT),
+				Entry("DELETE", DELETE),
+
+				// Response
+				Entry("CoapCodeEmpty", CoapCodeEmpty),
+				Entry("CoapCodeCreated", CoapCodeCreated),
+				Entry("CoapCodeDeleted", CoapCodeDeleted),
+				Entry("CoapCodeValid", CoapCodeValid),
+				Entry("CoapCodeChanged", CoapCodeChanged),
+				Entry("CoapCodeContent", CoapCodeContent),
+				Entry("CoapCodeContinue", CoapCodeContinue),
+
+				// Errors
+				Entry("CoapCodeBadRequest", CoapCodeBadRequest),
+				Entry("CoapCodeUnauthorized", CoapCodeUnauthorized),
+				Entry("CoapCodeBadOption", CoapCodeBadOption),
+				Entry("CoapCodeForbidden", CoapCodeForbidden),
+				Entry("CoapCodeNotFound", CoapCodeNotFound),
+				Entry("CoapCodeMethodNotAllowed", CoapCodeMethodNotAllowed),
+				Entry("CoapCodeNotAcceptable", CoapCodeNotAcceptable),
+				Entry("CoapCodeRequestEntityIncomplete", CoapCodeRequestEntityIncomplete),
+				Entry("CoapCodeConflict", CoapCodeConflict),
+				Entry("CoapCodePreconditionFailed", CoapCodePreconditionFailed),
+				Entry("CoapCodeRequestEntityTooLarge", CoapCodeRequestEntityTooLarge),
+				Entry("CoapCodeUnsupportedContentFormat", CoapCodeUnsupportedContentFormat),
+				Entry("CoapCodeInternalServerError", CoapCodeInternalServerError),
+				Entry("CoapCodeNotImplemented", CoapCodeNotImplemented),
+				Entry("CoapCodeBadGateway", CoapCodeBadGateway),
+				Entry("CoapCodeServiceUnavailable", CoapCodeServiceUnavailable),
+				Entry("CoapCodeGatewayTimeout", CoapCodeGatewayTimeout),
+				Entry("CoapCodeProxyingNotSupported", CoapCodeProxyingNotSupported),
+			)
+		})
+
+		Context("With correct Token", func() {
+			DescribeTable("Check each token by length",
+				func(tokenLength int) {
+					token := make([]byte, tokenLength)
+					rand.Read(token)
+					message.Token = token
+
+					datagram, err = Serialize(message)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(bytes.Equal(datagram[4:4+tokenLength], message.Token)).Should(BeTrue())
+				},
+				Entry("Token length is minimum", 1),
+				Entry("Token length is valid", 5),
+				Entry("Token length is maximum", 8),
+				// Entry("Token length is out of range", 9, false),
 			)
 		})
 	})
