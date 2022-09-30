89da85238ec73e8166ac5cf7a03f5e241ffed872
Alexey Spiridonov <forspall@gmail.com>
2022-09-30 13:28:59 +0700

    fix const

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	timeWait        = time.Millisecond * 1000
+	timeWait        = time.Millisecond * 400
 	maxSendAttempts = 6
 	sumTimeAttempts = timeWait*time.Duration(maxSendAttempts) + 100
 )
--- transport.go
+++ transport.go
@@ -332,9 +332,9 @@ func (sr *transport) sendPacketsToAddr(packets []*packet, windowsize int, shift 
 	}
 
 	var acked int
-	for i := shift; i < stop; i++ {
+	for i := 0; i < stop; i++ {
 		if !packets[i].acked {
-			if time.Since(packets[i].lastSend) >= time.Millisecond*300 {
+			if time.Since(packets[i].lastSend) >= timeWait {
 				if packets[i].attempts == maxSendAttempts {
 					MetricExpiredMessages.Inc()
 					return ErrMaxAttempts
@@ -519,7 +519,7 @@ func (sr *transport) sendARQBlock2ACK(input chan *CoAPMessage, message *CoAPMess
 					}
 				}
 			}
-		case <-time.After(timeWait / 5):
+		case <-time.After(sumTimeAttempts):
 			if err := sr.sendPacketsToAddr(packets, state.windowsize, shift, addr); err != nil {
 				return err
 			}
