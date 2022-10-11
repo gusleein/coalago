b22a5df9eead171ed67428213b999310ff53817c
Mednnn <mednyo@gmail.com>
2022-10-11 14:05:58 +0300

    add logs

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/lucas-clemente/aes12 v0.0.0-20171027163421-cd47fb39b79f
+	github.com/ndmsystems/logger v0.0.0-20221007121017-7f0a890a52db
 	github.com/onsi/ginkgo v1.14.2
 	github.com/onsi/gomega v1.10.3
 	github.com/patrickmn/go-cache v2.1.0+incompatible
--- localstate.go
+++ localstate.go
@@ -2,11 +2,11 @@ package coalago
 
 import (
 	"fmt"
+	log "github.com/ndmsystems/logger"
+	"github.com/patrickmn/go-cache"
 	"sync"
 	"sync/atomic"
 	"time"
-
-	"github.com/patrickmn/go-cache"
 )
 
 var StorageLocalStates = cache.New(sumTimeAttempts, time.Second)
@@ -18,6 +18,7 @@ func MakeLocalStateFn(r Resourcer, tr *transport, respHandler func(*CoAPMessage,
 	var bufBlock1 = make(map[int][]byte)
 	var totalBlocks1 = -1
 	var runnedHandler int32 = 0
+	var downloadingStartTime = time.Now()
 
 	return func(message *CoAPMessage) {
 		mx.Lock()
@@ -41,6 +42,11 @@ func MakeLocalStateFn(r Resourcer, tr *transport, respHandler func(*CoAPMessage,
 
 			requestOnReceive(r.getResourceForPathAndMethod(message.GetURIPath(), message.GetMethod()), tr, message)
 			closeCallback()
+			if len(bufBlock1) > 0 {
+				log.Info(fmt.Sprintf("Download speed : %d MBits, Data size : %d",
+					int64(len(bufBlock1)*MAX_PAYLOAD_SIZE)*time.Second.Milliseconds()/time.Since(downloadingStartTime).Milliseconds()/MBIT),
+					len(bufBlock1)*MAX_PAYLOAD_SIZE)
+			}
 		}
 
 		totalBlocks1, bufBlock1 = localStateMessageHandlerSelector(tr, totalBlocks1, bufBlock1, message, respHandler)
@@ -162,7 +168,6 @@ func localStateReceiveARQBlock1(sr *transport, totalBlocks int, buf map[int][]by
 			b = append(b, buf[i]...)
 		}
 		inputMessage.Payload = NewBytesPayload(b)
-
 		return true, totalBlocks, buf, inputMessage, nil
 	}
 
