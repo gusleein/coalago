5dbe365544723ba485b3b413de7cbd5906fc2dd3
Alexey Spiridonov <forspall@gmail.com>
2023-01-23 19:36:06 +0700

    small fixes

--- version.sh
+++ version.sh
@@ -3,13 +3,3 @@ version=`git describe --tags --long`
 # Write out the package.
 cat << EOF > version.go
 package coalago
-
-//go:generate bash ./version.sh
-var Version = "$version"
-EOF
-
-2022-11-02 03:48:00 > IpWan  { "current-id": "GigabitEthernet1", "description": "At-Hom", "id": "GigabitEthernet1", "order": "0", "priority": "65502", "state": "up" }
-
-2022-11-02 03:48:00 > IpWan  { "current-id": "Wireguard1", "description": "At-Hom", "id": "GigabitEthernet1", "order": "0", "priority": "65502", "state": "down" }
-
-2022-11-02 03:48:00 > IpWan  { "current-id": "Wireguard1", "description": "At-Hom", "id": "GigabitEthernet1", "order": "0", "priority": "65502", "state": "down" }
